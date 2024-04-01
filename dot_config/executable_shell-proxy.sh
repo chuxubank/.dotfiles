28d95f66eb7e56f1ad9e0b7cc4bde2a6a8a52935
Misaka <chuxubank@qq.com>
2024-04-01 21:39:30 +0800

    Use shell-proxy

---
+++ dot_config/executable_shell-proxy.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+location=$(networksetup -getcurrentlocation)
+
+if [[ "$location" = 'Work' ]]; then
+  echo "http://127.0.0.1:9000" # ZS
+else
+  echo "socks5://127.0.0.1:10800" # Xray
+fi
+
+# No Proxy
+echo "localhost,127.0.0.1"
